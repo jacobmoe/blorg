8ea47010deeacc34296d4f75eb8cb3fffc94892c
Jacob Moeller <jacobmoe@gmail.com>
2014-04-18 20:05:48 -0400

    allow text with no title

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -5,7 +5,6 @@ define([
   'router', 
 ], function($, _, Backbone, Router){
   var initialize = function(){
-    // Pass in our Router module and call it's initialize function
     Router.initialize();
   }
 
@@ -14,14 +13,3 @@ define([
   };
 });
 
-// ---- views ----------------------------------------------------------------
-// var NavSidebar = Backbone.View.extend({
-//   el: '.nav-sidebar',
-//   render: function () {
-//     var template = _.template($('#nav-sidebar-template').html(), {});
-//     this.$el.html(template);
-//   }
-// });
-
-
-// ---- routes ---------------------------------------------------------------
--- model/page.go
+++ model/page.go
@@ -12,13 +12,15 @@ type Page struct {
 // - the page name is a level-1 headline (*)
 // - the title of the post is a level-2 headline (**)
 // - the date of the post is a level-3 headline (***)
-// - the body of the post is contained under the date
+// - the body of the post is collected from all sections
 func PagesFromTree(tree *gorg.Tree) []*Page {
 	var pages []*Page
 
 	for _, subtree := range tree.Subtrees {
 		page := &Page{}
 		post := &Post{}
+
+		var sections []string
 
 		for _, node := range subtree.Nodes {
 
@@ -30,8 +32,9 @@ func PagesFromTree(tree *gorg.Tree) []*Page {
 				post.Title = node.Headline
 			case 3:
 				post.Date = node.Headline
-				post.Section = node.Section
 			}
+
+			sections = append(sections, node.Section...)
 
 			for _, sst := range subtree.Subtrees {
 				post := &Post{}
@@ -42,14 +45,18 @@ func PagesFromTree(tree *gorg.Tree) []*Page {
 						post.Title = n.Headline
 					case 3:
 						post.Date = n.Headline
-						post.Section = n.Section
 					}
+
+					sections = append(sections, n.Section...)
 				}
 
+				post.Section = sections
+				sections = []string{}
 				page.Posts = append(page.Posts, post)
 			}
 		}
 
+		post.Section = sections
 		page.Posts = append(page.Posts, post)
 		pages = append(pages, page)
 	}
